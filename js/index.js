7f3e5c720bd2eedb301f0131b5a5756312979961
Dona Adhiambo <donaathiambo2017@gmail.com>
2017-12-01 14:52:58 +0300

    commit changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-       $(".clickable").click(function() {
-         $("#drop").toggle();
-         $("#drop").toggle();
-       });
-     });
-   
+        $("p").click(function() {
+          $("img").show();
+        });
+      });
+
+$(document).ready(function() {
+$("p").click(function() {
+$(".gift-showing").show();
+$(".gift-hidden").hide();
+    });
+  });
+$(document).ready(function() {
+$("p").click(function() {
+$(".gift-showing").toggle();
+$(".gift-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+$(".clickable").click(function() {
+$(".gift-showing").toggle();
+$(".gift-hidden").toggle();
+  });
+});
